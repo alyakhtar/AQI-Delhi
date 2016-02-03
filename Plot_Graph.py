62cf4ba8df0717000d685b9cfd7e28a03f3f5bb1
alyakhtar <samurai.aly@gmail.com>
2016-02-03 08:11:19 -0800

    Compared PM2.5 trends on graph
    Signed-off-by: alyakhtar <samurai.aly@gmail.com>

---
+++ Plot_Graph.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def data_2013():
+    i = 0
+    main_avg = []
+    for a in pd.read_csv('aqm2013.csv', chunksize=24):
+        add = 0
+        avg = 0.0
+        data = []
+        df = pd.DataFrame(data=a)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for pm in data:
+            if type(pm) is long or type(pm) is float:
+                var = pm
+                add = add + var
+            elif type(pm) is str:
+                if pm != 'NoData' and pm != 'PwrFail':
+                    var = float(pm)
+                    add = add + var
+        avg = add/24
+        i += 1
+        # if avg == 0.0:
+        # 	print "No Data", i
+        # else:
+        # 	print round(avg,2),i
+        main_avg.append(avg)
+
+    # new1 = pd.DataFrame(main_avg)
+    # new1.columns = ['2013']
+    return main_avg
+
+
+def data_2014():
+    i = 0
+    main_avg = []
+    for a in pd.read_csv('aqm2014.csv', chunksize=24):
+        add = 0
+        avg = 0.0
+        data = []
+        df = pd.DataFrame(data=a)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for pm in data:
+            if type(pm) is long or type(pm) is float:
+                var = pm
+                add = add + var
+            elif type(pm) is str:
+                if pm != 'NoData' and pm != 'PwrFail':
+                    var = float(pm)
+                    add = add + var
+        avg = add/24
+        i += 1
+
+        # if avg == 0.0:
+        # 	print "No Data", i
+        # else:
+        # 	print round(avg,2),i
+        main_avg.append(avg)
+
+    # new2 = pd.DataFrame(main_avg)
+    # new2.columns = ['2014']
+    return main_avg
+
+
+def data_2015():
+    i = 0
+    main_avg = []
+    for a in pd.read_csv('aqm2015.csv', chunksize=24):
+        add = 0
+        avg = 0.0
+        data = []
+        df = pd.DataFrame(data=a)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for pm in data:
+            if type(pm) is long or type(pm) is float:
+                var = pm
+                add = add + var
+            elif type(pm) is str:
+                if pm != 'NoData' and pm != '---' and pm != 'InVld' and pm != 'PwrFail':
+                    var = float(pm)
+                    add = add + var
+        avg = add/24
+        i += 1
+
+        # if avg == 0.0:
+        # 	print "No Data", i
+        # else:
+        # 	print round(avg,2),i
+        main_avg.append(avg)
+
+    # new3 = pd.DataFrame(main_avg)
+    # new3.columns = ['2015']
+    return main_avg
+
+
+if __name__ == "__main__":
+    a = []
+    b = []
+    a = data_2013()
+    b = data_2014()
+    c = data_2015()
+    plt.plot(xrange(0, 365), a, label='2013')
+    plt.plot(xrange(0, 364), b, label='2014')
+    plt.plot(xrange(0, 365), c, label='2015')
+    plt.xlabel('Day')
+    plt.ylabel('PM 2.5')
+    plt.legend(loc='upper left')
+    plt.show()
