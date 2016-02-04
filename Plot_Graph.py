fb11dde38a8c9a527b592d6c6cafc5a0c21f6c6e
alyakhtar <samurai.aly@gmail.com>
2016-02-03 18:36:43 -0800

    Jan data added
    Signed-off-by: alyakhtar <samurai.aly@gmail.com>

--- Plot_Graph.py
+++ Plot_Graph.py
@@ -96,6 +96,36 @@ def data_2015():
     # new3.columns = ['2015']
     return main_avg
 
+def data_2016():
+    i = 0
+    main_avg = []
+    for a in pd.read_csv('aqm2016.csv', chunksize=24):
+        add = 0
+        avg = 0.0
+        data = []
+        df = pd.DataFrame(data=a)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for pm in data:
+            if type(pm) is long or type(pm) is float:
+                var = pm
+                add = add + var
+            elif type(pm) is str:
+                if pm != 'NoData' and pm != '---' and pm != 'InVld' and pm != 'PwrFail':
+                    var = float(pm)
+                    add = add + var
+        avg = add/24
+        i += 1
+
+        # if avg == 0.0:
+        # 	print "No Data", i
+        # else:
+        # 	print round(avg,2),i
+        main_avg.append(avg)
+
+    # new3 = pd.DataFrame(main_avg)
+    # new3.columns = ['2015']
+    return main_avg
 
 if __name__ == "__main__":
     a = []
@@ -103,10 +133,12 @@ if __name__ == "__main__":
     a = data_2013()
     b = data_2014()
     c = data_2015()
+    d = data_2016() 
     plt.plot(xrange(0, 365), a, label='2013')
     plt.plot(xrange(0, 364), b, label='2014')
     plt.plot(xrange(0, 365), c, label='2015')
+    plt.plot(xrange(0, 31), d, label='2016')
     plt.xlabel('Day')
     plt.ylabel('PM 2.5')
-    plt.legend(loc='upper left')
+    plt.legend(loc='upper right')
     plt.show()
