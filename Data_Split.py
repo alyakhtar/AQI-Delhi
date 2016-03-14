b9e2d7c1382beda16abdd894f83ef73638006b3f
aly akhtar <samurai.aly@gmail.com>
2016-03-14 18:46:47 +0530

    Data Combining
    Signed-off-by: aly akhtar <samurai.aly@gmail.com>

--- Data_Split.py
+++ Data_Split.py
@@ -38,8 +38,43 @@ def split(year):
         wr.writerow(
             ['SNO', 'T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM', 'PM 2.5'])
         wr.writerows(mylist_test)
+def combine_train(year,cs):
+    for a in pd.read_csv('Train/Train_'+str(year)+'.csv', chunksize=cs):
+        df = pd.DataFrame(data=a)
+        mylist = df.values.tolist()
+    return mylist
+
+def combine_test(year,cs):
+    for a in pd.read_csv('Test/Test_'+str(year)+'.csv', chunksize=cs):
+        df = pd.DataFrame(data=a)
+        mylist = df.values.tolist()
+    return mylist
+
 
 
 if __name__ == "__main__":
     for year in xrange(2013, 2017):
         split(year)
+
+    a = combine_train(2013,343)
+    b = combine_train(2014,346)
+    c = combine_train(2015,349)
+    d = combine_train(2016,59)
+
+    final_train = a+b+c+d
+    with open('Train/Train_Combine.csv', 'w') as csvfile:
+        wr = csv.writer(csvfile, dialect='excel')
+        wr.writerow(['SNO', 'T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM', 'PM 2.5'])
+        wr.writerows(final_train)
+
+
+    a = combine_test(2013,343)
+    b = combine_test(2014,346)
+    c = combine_test(2015,349)
+    d = combine_test(2016,59)
+
+    final_test = a+b+c+d
+    with open('Test/Test_Combine.csv', 'w') as csvfile:
+        wr = csv.writer(csvfile, dialect='excel')
+        wr.writerow(['SNO', 'T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM', 'PM 2.5'])
+        wr.writerows(final_test)
