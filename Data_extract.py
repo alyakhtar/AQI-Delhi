135acb389a623e7255089d9ac960677650ef4472
iCHAIT <cgupta319@gmail.com>
2016-03-14 14:18:56 +0530

    original data with no outliers

--- Data_extract.py
+++ Data_extract.py
@@ -3,7 +3,7 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 from tabulate import tabulate
-from test import data_2013, data_2014, data_2015,data_2016
+from Plot_Graph import data_2013, data_2014, data_2015, data_2016
 
 
 def met_data(month, year):
@@ -53,7 +53,8 @@ if __name__ == "__main__":
         final = []
         with open('met_'+str(year)+'.csv', 'w') as csvfile:
             wr = csv.writer(csvfile, dialect='excel')
-            wr.writerow(['SNO', 'T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM', 'PM 2.5' , 'NORMALISED'])
+            wr.writerow(
+                ['SNO', 'T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM', 'PM 2.5'])
         for month in xrange(1, 13):
             if year == 2016:
                 if month < 3:
@@ -74,42 +75,13 @@ if __name__ == "__main__":
             final[i].insert(0, i+1)
             final[i].insert(9, pm[i])
 
-        maxi = 0.0
-        for a in final:
-            if a[9] != '-':
-                if type(a[9]) is str:
-                    abc = float(a[9])
-                abc = a[9]
-                if abc > maxi:
-                    maxi = a[9]
-        mini = maxi
-        for a in final:
-            if a[9] != '-':
-                if type(a[9]) is str:
-                    abc = float(a[9])
-                abc = a[9]
-                if abc < mini:
-                    mini = a[9]
-       
-        i = 0
-        maxi = float(maxi)
-        mini = float(mini)
-        # print maxi, mini
-        for a in final:
-            if a[9] is '-':
-                y = '-'
-            else:
-                y = (((a[9] - mini) / (maxi - mini)) * 2) - 1
-            final[i].append(y)
-            i = i + 1
-
-        # print year
-
-        # print tabulate(final, headers=['SNO', 'T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM', 'PM 2.5','Normalised'], tablefmt='fancy_grid')
-        # print year
-        # break
-
         with open('met_'+str(year)+'.csv', 'a') as csvfile:
             wr = csv.writer(csvfile, dialect='excel')
-            wr.writerows(final)
-
+            for row in final:
+                flag = 0
+                for elem in row:
+                    if elem == 0 or elem == "-":
+                        flag = 1
+                        print "Error", elem, row
+                if flag != 1:
+                    wr.writerow(row)
--- Fetch_met_data.py
+++ Fetch_met_data.py
@@ -2,43 +2,48 @@ import csv
 import requests
 import sys
 from bs4 import BeautifulSoup
-import os,time
+import os
+import time
 
 
 def fetch():
     done = 2.63
     i = 1
-    for year in xrange(2013,2017):
-        for month in xrange(1,13):
+    for year in xrange(2013, 2017):
+        for month in xrange(1, 13):
 
             if year == 2016:
                 if month < 3:
-                    url = 'http://en.tutiempo.net/climate/0%i-%i/ws-421820.html' %(month,year)
+                    url = 'http://en.tutiempo.net/climate/0%i-%i/ws-421820.html' % (
+                        month, year)
                 else:
                     break
-            else:           
-                if month <10:
-                    url = 'http://en.tutiempo.net/climate/0%i-%i/ws-421820.html' %(month,year)
+            else:
+                if month < 10:
+                    url = 'http://en.tutiempo.net/climate/0%i-%i/ws-421820.html' % (
+                        month, year)
                 else:
-                    url = 'http://en.tutiempo.net/climate/%i-%i/ws-421820.html' %(month,year)
+                    url = 'http://en.tutiempo.net/climate/%i-%i/ws-421820.html' % (
+                        month, year)
 
             source_code = requests.get(url)
 
             plain_text = source_code.text.encode('utf-8')
 
-            if not os.path.exists("%i" %year):
-                os.makedirs("%i" %year)
+            if not os.path.exists("%i" % year):
+                os.makedirs("%i" % year)
 
-            with open("%i/%i.html" %(year,month), "w") as output_file:
+            with open("%i/%i.html" % (year, month), "w") as output_file:
                 output_file.write(plain_text)
 
             if i == 37:
                 done = 98
 
-            sys.stdout.write("\r[%s%s] %d%% Completed" % ('=' * i, ' ' * (38-i), done))
+            sys.stdout.write("\r[%s%s] %d%% Completed" %
+                             ('=' * i, ' ' * (38-i), done))
             done = done + 2.63
             i += 1
-            
+
             sys.stdout.flush()
 
 
@@ -46,7 +51,7 @@ if __name__ == "__main__":
     start = time.time()
     fetch()
     end = time.time()
-    print '\nTime Taken : ',end-start, 'seconds'
+    print '\nTime Taken : ', end-start, 'seconds'
 
 # with open('test.csv', 'wb') as fp:
 #     a = csv.writer(fp, delimiter=',')
