38cbda1af6e5f6f158dac047e300e2204e90beb6
aly akhtar <samurai.aly@gmail.com>
2016-03-18 18:50:24 +0530

    Confusion Matrix created
    Signed-off-by: aly akhtar <samurai.aly@gmail.com>

---
+++ Confuse.py
@@ -0,0 +1,56 @@
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(4)
+    plt.xticks(tick_marks, ['0', '1', '2' , '3'], rotation=45)
+    plt.yticks(tick_marks, ['0', '1', '2' , '3'])
+    plt.tight_layout()
+    plt.ylabel('Expected ')
+    plt.xlabel('Observed')
+    plt.show()
+
+
+def confuse(Observed,Expected):
+	A_O = []
+	A_E = []
+
+
+	for i in Observed:
+		if i >= -1.0 and i < -0.5:
+			A_O.append(0)
+		elif i >= -0.5 and i < 0:
+			A_O.append(1)
+		elif i >= 0 and i < 0.5:
+			A_O.append(2)
+		else:
+			A_O.append(3)
+
+	for i in Expected:
+		if i >= -1.0 and i < -0.5:
+			A_E.append(0)
+		elif i >= -0.5 and i < 0:
+			A_E.append(1)
+		elif i >= 0 and i < 0.5:
+			A_E.append(2)
+		else:
+			A_E.append(3)
+
+	return confusion_matrix(A_E, A_O)
+
+def main(x,y):
+	sum = 0.0
+	a = confuse(x,y)
+	plot_confusion_matrix(a)
+	for i in xrange(4):
+		for j in xrange(4): 
+			if i == j:
+				sum += a[i][j]
+
+	print "Accuracy : ",(sum/441)*100
+
--- mlp.py
+++ mlp.py
@@ -1,8 +1,10 @@
-from keras.models import Sequential
+from keras.models import Sequential,Graph
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
 import pandas as pd
-import time
+import time,math
+import matplotlib.pyplot as plt
+from Confuse import main
 
 X = pd.read_csv('Train/Train_Combine.csv', usecols=[
                 'T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM'])
@@ -10,7 +12,6 @@ Y = pd.read_csv('Train/Train_Combine.csv', usecols=['PM 2.5'])
 
 X = X.values
 Y = Y.values
-# Y = np.asarray(Y,dtype="|S6")
 
 X2 = pd.read_csv('Test/Test_Combine.csv', usecols=[
                  'T', 'TM', 'Tm', 'SLP', 'H', 'VV', 'V', 'VM'])
@@ -20,9 +21,7 @@ X2 = X2.values
 Y2 = Y2.values
 
 model = Sequential()
-# Dense(64) is a fully-connected layer with 64 hidden units.
-# in the first layer, you must specify the expected input data shape:
-# here, 20-dimensional vectors.
+
 model.add(Dense(10, input_dim=8, init='uniform'))
 model.add(Activation('tanh'))
 model.add(Dense(10,input_dim=10, init='uniform'))
@@ -31,18 +30,20 @@ model.add(Dense(1,input_dim=10, init='uniform'))
 model.add(Activation('tanh'))
 
 sgd = SGD(lr=0.1, decay=1e-3, momentum=0.5, nesterov=True)
-model.compile(loss='mean_squared_error',
-              optimizer=sgd)
+model.compile(loss='mse',optimizer=sgd)
 
-model.fit(X, Y,
-          nb_epoch=20,
-          batch_size=1,
-          show_accuracy=True)
+model.fit(X, Y,nb_epoch=20,batch_size=1,show_accuracy=False)
 score = model.evaluate(X2, Y2, batch_size=1)
 preds = model.predict(X2, batch_size=1, verbose=0)
 
 
-print "Error: ",score*100
-print "Accuracy: ", (1-score)*100
-# time.sleep(2)
-print preds[:10],Y2[:10]
+main(preds,Y2)
+
+plt.plot(xrange(0, 441), preds, label='Observed')
+plt.plot(xrange(0, 441), Y2, label='Expected')
+plt.xlabel('Data Points')
+plt.ylabel('PM 2.5')
+plt.legend(loc='upper right')
+plt.show()
+
+print "Error: ",score
